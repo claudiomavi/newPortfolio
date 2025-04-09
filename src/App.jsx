14178be3f5dfe75b1d3eceb33923b93705437b3f
Claudio <claudio.marino@outlook.com>
2025-04-09 20:43:46 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import Layout from "./Components/Layout";
 import Projects from "./pages/Projects";
 
