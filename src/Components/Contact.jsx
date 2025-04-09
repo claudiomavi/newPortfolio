5b3c133b849855c4958d5024162807ffea19cbbf
Claudio <claudio.marino@outlook.com>
2025-04-09 16:33:28 +0200

    changed to tailwind

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,12 @@
 export default function Contact() {
   return (
-    <section id="contact" className="contact not-md:py-32 dynamicBg py-48">
+    <section id="contact" className="contact not-md:py-32 py-48">
       <div className="mx-auto w-[92%] max-w-[120rem]">
         <h2 className="not-md:mb-32 mb-36">
           <span className="relative mb-14 block text-center text-6xl uppercase tracking-widest text-white">
             Contact
           </span>
-          <span className="not-sm:text-3xl m-auto block max-w-7xl text-center text-3xl font-medium text-[#eee]">
+          <span className="not-sm:text-3xl leading-12 m-auto block max-w-7xl text-center text-3xl font-medium text-[#eee]">
             Please reach out if you have any questions! I'm happy to jump on a
             video call to brainstorm projects and ideas. Send me an email at
             claudio.marino@outlook.es or call me directly at +34 662 502 171
