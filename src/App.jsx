bddb618566aebf410a3b7378246e085ec2c40320
claudiomavi <118379948+claudiomavi@users.noreply.github.com>
2025-04-10 11:41:56 +0000

    updated the todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Home from "./pages/Home";
 import Layout from "./Components/Layout";
 import Projects from "./pages/Projects";
 
-// to-do: sustituir con react hash
-// to-do: solucionar error pagina projects
+// to-do: instalar react-router-hash-link
+// to-do: crear componente scroll to top y usarlo
+// to-do: usa hashlink en el navbar
+// to-do: elimina la función de scroll into view de ProjectsSection
+// to-do: elimina la función handleScroll de Header
 
 export default function App() {
   return (
