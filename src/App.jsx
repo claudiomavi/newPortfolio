3e802edd229c7c3398c1a7770df813fb18b6c21f
Claudio <claudio.marino@outlook.com>
2025-04-10 16:43:16 +0200

    agregado componente ScrollToTop para gestionar el desplazamiento al inicio de la página

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./Components/Layout";
 import Projects from "./pages/Projects";
-
-// to-do: instalar react-router-hash-link
-// to-do: crear componente scroll to top y usarlo
-// to-do: usa hashlink en el navbar
-// to-do: elimina la función de scroll into view de ProjectsSection
-// to-do: elimina la función handleScroll de Header
+import ScrollToTop from "./Components/ScrollToTop";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="projects" element={<Projects />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
